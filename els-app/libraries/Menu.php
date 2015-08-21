d106df92fbb027ce7291d4596d8d59b6f4a3fd28
sagormax <sagor.touch@gmail.com>
2015-08-21 12:23:00 +0600

    Complete multi menu library

--- els-app/libraries/Menu.php
+++ els-app/libraries/Menu.php
@@ -6,88 +6,213 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 class Menu{
 
-	public function generateFrontendMenu($attr)
+	public function generateMenu($attr)
 	{
-	 	$CI =& get_instance();
-	    $CI->load->model('usersModel', 'umodel');
+	 	$CI 					=& get_instance(); // Get CI Instance
+	 	$table_name 			= $CI->security->xss_clean($attr['table_name']); // Receive table name
 
-	    $all_list = $CI->db->get('ep_menu')->result();
+	 	if( $CI->db->table_exists( $table_name ) ) // Checking table
+	 	{
+			return $this->menuProcess( $attr, $table_name );
+	 	}
+	 	else
+	 	{
+	 		$CI->load->dbforge();
 
-		$menu_data = $all_list;
-		$ul_class = "nav nav-bar";
-		$ul_ID = "nav";
-		$li_class = "item_list";
-		$ul_submenu_class = "secondary_menu";
-		$ul_submenu_ID = "secondary";
-		$custom_child_class = "";
-		$output = "";
+			$fields = array(
+		        'ID' => array(
+	                 'type' => 'bigint',
+	                 'constraint' => 20,
+	                 'auto_increment' => TRUE
+	            ),
+		        'menu_cat_id' => array(
+                     'type' => 'bigint',
+                     'constraint' => '20',
+                ),
+		        'menu_cat_name' => array(
+                     'type' =>'VARCHAR',
+                     'constraint' => '200',
+                ),
+		        'menu_name' => array(
+                     'type' =>'VARCHAR',
+                     'constraint' => '200',
+                ),
+		        'menu_parent' => array(
+                     'type' => 'bigint',
+                     'constraint' => '20',
+                ),
+		        'menu_url' => array(
+                     'type' =>'VARCHAR',
+                     'constraint' => '200',
+                ),
+			);
+			$CI->dbforge->add_key('ID', TRUE); // Set Primary Key
+			$CI->dbforge->add_field($fields); // Set fields
+	 		$CI->dbforge->create_table($table_name, TRUE); // Creating Table
 
-		$output .= '<ul class="'.$ul_class.' els-menu" id="'.$ul_ID.'">';
+	 		/**
+	 		* Insert sample data
+	 		*/
+	 		$sampleData1 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'HTML',
+	 			'menu_parent'		=> '0',
+	 			'menu_url'			=> '#',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData1);
+	 		$sampleData2 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'CSS',
+	 			'menu_parent'		=> '0',
+	 			'menu_url'			=> '#',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData2);
+	 		$sampleData3 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'Body',
+	 			'menu_parent'		=> '1',
+	 			'menu_url'			=> 'http://www.google.com',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData3);
+	 		$sampleData4 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'Color',
+	 			'menu_parent'		=> '3',
+	 			'menu_url'			=> '#',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData4);
+	 		$sampleData5 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'Style',
+	 			'menu_parent'		=> '2',
+	 			'menu_url'			=> 'http://www.facebook.com',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData5);
+	 		$sampleData6 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'Pink',
+	 			'menu_parent'		=> '4',
+	 			'menu_url'			=> 'http://www.color.com',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData6);
+	 		$sampleData7 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'About',
+	 			'menu_parent'		=> '0',
+	 			'menu_url'			=> 'http://www.about.com',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData7);
+	 		$sampleData8 = array(
+	 			'menu_cat_id'		=> 1,
+	 			'menu_cat_name'		=> 'Header Menu',
+	 			'menu_name'			=> 'Contact',
+	 			'menu_parent'		=> '0',
+	 			'menu_url'			=> 'http://www.contact.com',
+	 		);
+	 		$CI->db->insert($table_name, $sampleData8);
+
+
+	 		return $this->menuProcess( $attr, $table_name ); // After creating table call again menuProcess
+	 	}
+
+	}
+
+
+
+	// Checking data
+	private function menuProcess( $attr, $table_name )
+	{
+		$CI 					=& get_instance(); // Get CI Instance
+		$where					= array( 'menu_cat_id' => $attr['menu_cat_ID'] );
+	    $all_list 				= $CI->db->get_where($table_name, $where)->result(); // Get table all data
+
+	    if( empty($all_list) ) return "No data in this table" ;
+
+		$menu_data 				= $all_list;
+		$ul_class 				= $attr['ul_class'];
+		$ul_ID 					= $attr['ul_ID'];
+		$ul_attr 				= $attr['ul_attr'];
+		$li_class 				= $attr['li_class'];
+		$li_if_submenu_class	= $attr['li_if_submenu_class'];
+		$ul_submenu_class 		= $attr['ul_submenu_class'];
+		$ul_submenu_ID 			= $attr['ul_submenu_ID'];
+		$output 				= "";
+
+		$output .= '<ul class="'.$ul_class.' els-menu" id="'.$ul_ID.'" '.$ul_attr.'>';
 		foreach ($menu_data as $menu)
 		{
-		    $id = $menu->ID;
-		    $parent_id = $menu->menu_parent;
-		    $name = $menu->menu_name;
-		    if($parent_id != 0) continue;
+		    $id 		= $menu->ID;
+		    $parent_id 	= $menu->menu_parent;
+		    $name 		= $menu->menu_name;
+		    $url 		= $menu->menu_url;
 
-		    $output .= '<li class="'.$li_class.'">'.$name;
-		    $child = $this->has_child($id, $menu_data, $custom_child_class);
+		    if($parent_id != 0) continue; // stop repeating
+
+		    $child = $this->has_child($id, $menu_data, $li_if_submenu_class, $ul_submenu_class);
 		    if($child)
 		    {
-		      $output .= '<ul class="submenu">';
-		        $output .= $child;
-		      $output .= "</ul>";
+		      $output .= '<li class="'.$li_if_submenu_class.' '.$li_class.'"><a href="'.$url.'">'.$name.'</a>';
+			      $output .= '<ul class="'.$ul_submenu_class.'" id="'.$ul_submenu_ID.'">';
+			        $output .= $child;
+			      $output .= "</ul>";
+			  $output .= "</li>";
 		    }
-		    $output .= "</li>";
+		    else
+		    {
+		      $output .= '<li class="'.$li_class.'"><a href="'.$url.'">'.$name.'</a></li>';
+		    }
 
 		}
 		$output .= '</ul>';
 		return $output;
 
+
 	}
 
 
-	// Generate child Level 1 list
-	public function has_child($id, $menu_data, $custom_child_class)
+
+
+	// Generate childs list
+	private function has_child($id, $menu_data, $li_if_submenu_class, $ul_submenu_class)
 	{
-	    $child_class = $custom_child_class." child_list_level_1";
-	    $child_data = "";
-	    $child_2_data = "";
+	    $child_class 	= $li_if_submenu_class." child-list";
+	    $child_data 	= "";
+	    $child_tree 	= ""; // Get under the child data
 	    foreach ($menu_data as $child)
 	    {
-	        $child_id = $child->ID;
-	        $child_parent_id = $child->menu_parent;
+	        $child_id 			= $child->ID; // current child ID
+	        $child_parent_id 	= $child->menu_parent; // current child parent ID
+	        $url 				= $child->menu_url;
 	        if( $child_parent_id == $id )
-	          $child_data .='<li class="'.$child_class.'">'.$child->menu_name;
+	        {
+	          	// Recursive call for get child tree data
+			    $results = $this->has_child($child_id, $menu_data, $li_if_submenu_class, $ul_submenu_class);
 
-			    $child_2 = $this->has_child_2($child_id, $menu_data);
-			    if($child_2)
+			    if($results)
 			    {
-			      $child_2_data .= '<ul class="submenu level_2">';
-			        $child_2_data .= $child_2;
-			      $child_2_data .= "</ul>";
+			      $child_tree .= '<li class="'.$child_class.'"><a href="'.$url.'">'.$child->menu_name.'</a>';
+				      $child_tree .= '<ul class="'.$ul_submenu_class.'">';
+				        $child_tree .= $results;
+				      $child_tree .= "</ul>";
+				  $child_tree .= "</li>";
+				  $child_data .= $child_tree; // concate under the child all data
 			    }
-			    //$child_data .= $child_2_data;
-			    $child_data .= "</li>";
+			    else
+			    {
+			     $child_data .='<li class="child-list"><a href="'.$url.'">'.$child->menu_name.'</a></li>';
+			    }
 
+			}
 	    }
 	    return $child_data;
 	}
 
-
-	// Generate child 2nd level list
-	public function has_child_2($child_id, $menu_data)
-	{
-	    $child_class = " child_list_level_2";
-	    $child_data_2 = "";
-	    foreach ($menu_data as $child_2)
-	    {
-	        $child_parent_id_2 = $child_2->menu_parent;
-	        if( $child_parent_id_2 == $child_id )
-	          $child_data_2 .='<li class="'.$child_class.'">'.$child_2->menu_name.'</li>';
-
-	    }
-	    return $child_data_2;
-	}
 
 }
--- els-app/modules/dashboard/views/homeView.php
+++ els-app/modules/dashboard/views/homeView.php
@@ -4,18 +4,69 @@
   </div>
 <!--End-breadcrumbs-->
 
-<?php
-    $attr = array(
-      'table_name' => 'ep_menu',
-      'ul_class' => "nav nav-bar",
-      'ul_ID' => "nav",
-      'li_class' => "item_list",
-      'ul_submenu_class' => "secondary_menu",
-      'ul_submenu_ID' => "secondary",
-      'custom_child_class' => "",
-    );
-    echo $this->menu->generateFrontendMenu($attr);
-?>
+<div class="container">
+  <div class="row">
+      <h2>CodeIgniter Multi level dropdown menu in Bootstrap 3</h2>
+      <hr>
+      <div class="dropdown">
+          <a id="dLabel" role="button" data-toggle="dropdown" class="btn btn-primary" data-target="#" href="/page.html">
+              Dropdown <span class="caret"></span>
+          </a>
+
+          <?php
+              $attr = array(
+                'table_name'          => 'ep-menu',
+                'menu_cat_ID'         =>  1,
+                'ul_class'            => "dropdown-menu multi-level",
+                'ul_ID'               => "nav",
+                'ul_attr'             => 'role="menu" aria-labelledby="dropdownMenu"',
+                'li_class'            => "item-list",
+                'li_if_submenu_class' => "dropdown-submenu",
+                'ul_submenu_class'    => "dropdown-menu",
+                'ul_submenu_ID'       => "secondary",
+              );
+              echo $this->menu->generateMenu($attr);
+          ?>
+      </div>
+  </div>
+</div>
+
+
+
+
+<div class="container">
+  <div class="row">
+      <h2>Multi level dropdown menu in Bootstrap 3</h2>
+      <hr>
+      <div class="dropdown">
+          <a id="dLabel" role="button" data-toggle="dropdown" class="btn btn-primary" data-target="#" href="/page.html">
+              Dropdown <span class="caret"></span>
+          </a>
+
+          <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+            <li><a href="#">Some action</a></li>
+            <li><a href="#">Some other action</a></li>
+            <li class="divider"></li>
+            <li class="dropdown-submenu">
+              <a tabindex="-1" href="#">Hover me for more options</a>
+              <ul class="dropdown-menu">
+                <li><a tabindex="-1" href="#">Second level</a></li>
+                <li class="dropdown-submenu">
+                  <a href="#">Even More..</a>
+                  <ul class="dropdown-menu">
+                      <li><a href="#">3rd level</a></li>
+                    <li><a href="#">3rd level</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Second level</a></li>
+                <li><a href="#">Second level</a></li>
+              </ul>
+            </li>
+          </ul>
+      </div>
+  </div>
+</div>
+
 
 <!--Action boxes-->
   <div class="container-fluid">
